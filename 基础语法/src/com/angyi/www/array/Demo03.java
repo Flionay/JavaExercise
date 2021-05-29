7f30d55d70fe7f728db91e840acab96ab59a2104
angyi <angyi_jq@163.com>
2021-05-29 20:25:19 +0800

    20210531

---
+++ 基础语法/src/com/angyi/www/array/Demo03.java
@@ -0,0 +1,19 @@
+package com.angyi.www.array;
+
+public class Demo03 {
+    public static void main(String[] args) {
+        int[] array = {1,2,3,4,5,};
+
+        // 增强for循环
+        for (int i : array) {
+            System.out.println(i);
+        }
+    }
+
+//    public static int[] reverse(int[] arrays){
+//        int res[] = new int[arrays.length];
+
+
+
+//    }
+}
